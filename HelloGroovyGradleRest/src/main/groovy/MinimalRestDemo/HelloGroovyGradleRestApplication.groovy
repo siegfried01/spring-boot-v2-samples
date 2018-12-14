42a219b149728ddc0d5f6659f8afd2b065fa994d
Siegfried Heintze <siegfried@heintze.com>
2018-12-13 18:13:56 -0800

    virgin initializr

---
+++ HelloGroovyGradleRest/src/main/groovy/MinimalRestDemo/HelloGroovyGradleRestApplication.groovy
@@ -0,0 +1,14 @@
+package MinimalRestDemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HelloGroovyGradleRestApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run HelloGroovyGradleRestApplication, args
+	}
+
+}
+
